9999a69329b705c7d1badd1cdccc1ada4ee7ef8c
Rpal1315 <ritankar.pal@outlook.com>
2024-01-16 21:09:53 +0530

    Addition of Files

--- Dict_Programs/Dict_programs.py
+++ Dict_Programs/Dict_programs.py
@@ -1,15 +1,31 @@
 def DictMerge(d1,d2):
+    """
+    Merge two dictionaries.
+
+    Args:
+        d1 (dict): The first dictionary to be merged.
+        d2 (dict): The second dictionary to be merged.
+
+    Returns:
+        dict: The merged dictionary.
+
+    """
     d = d1.copy()
     d.update(d2)
     return d
 
 def DictSum(dct:dict):
     """
-    Calculates the sum of all values in a given dictionary.
+    Calculate the sum of all values in a dictionary.
 
-    Parameters:
-    - dct (dict): The dictionary to calculate the sum from.
+    Args:
+        dct (dict): The dictionary to calculate the sum of.
 
     Returns:
-    - The sum of all values in the dictionary.
+        int: The sum of all values in the dictionary.
     """
+    out = sum(dct.values())
+    return out
+
+
+DictSum({"Ram": 89, "Shyam": 99, "Jadu": 77, "Idrish": 78})
