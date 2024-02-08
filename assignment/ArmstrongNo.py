c03e949d37ad3fcc994ff5e83499f7791de3970a
Rpal1315 <ritankar.pal@outlook.com>
2024-02-08 21:19:33 +0530

    Addition of Files

---
+++ assignment/ArmstrongNo.py
@@ -0,0 +1,15 @@
+def ArmstrongNo(inp):
+    # inp = int(input("Enter a no:"))
+    leng = len(str(inp))
+    s = 0
+    for i in str(inp):
+        i = int(i)
+        s += i ** leng
+
+    if s == inp:
+        print("The no. is a Armstrong No.")
+    else:
+        print("The no. is not a Armstrong No.")
+
+
+ArmstrongNo(153)
